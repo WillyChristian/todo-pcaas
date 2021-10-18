ce23e833cd1a20f2d21c3662e0f37abcbbacb71d
WillyChristian <crisaort@gmail.com>
2021-10-18 01:20:36 -0300

    filx key property on each maped image

--- pages/components/modal/carousel/Carousel.js
+++ pages/components/modal/carousel/Carousel.js
@@ -9,7 +9,10 @@ export default function Carousel() {
     <>
       <section className="overflow-scrol">
         {imgs.map((image, index) => (
-          <div className="my-2 p-5 h-full w-full bg-gray-500 flex-col justify-center items-center">
+          <div
+            key={index}
+            className="my-2 p-5 h-full w-full bg-gray-500 flex-col justify-center items-center"
+          >
             <div className="container p-2 m-2">
               <h1 className="text-lg uppercase text-white">
                 Etapa {index + 1}
