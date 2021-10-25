21b6b2ec7cc944ba049c6444aef486c5940d2287
WillyChristian <crisaort@gmail.com>
2021-10-25 01:23:50 -0300

    small fixes

--- pages/todo.js
+++ pages/todo.js
@@ -1,13 +1,12 @@
 import React from "react";
-const config = require("./components/steps/config");
-// import { config } from "./components/steps/config";
+import config from "./components/steps/config";
 import Step from "./components/steps/Step";
 
 function Todo() {
   return (
     <>
       <div className="flex flex-col items-center justify-center w-full py-5 rounded-xl">
-        {config.map((step, index) => {
+        {config[0].map((step, index) => {
           return (
             <div key={index * 2} className="w-8/12 my-2">
               <Step step={step} index={index} />
