f7da6acd186572222d240b110820d41b505393e9
WillyChristian <crisaort@gmail.com>
2021-10-25 00:50:17 -0300

    add small fixes

--- pages/todo.js
+++ pages/todo.js
@@ -1,6 +1,6 @@
 import React from "react";
-
-import { config } from "./components/steps/config";
+const config = require("./components/steps/config");
+// import { config } from "./components/steps/config";
 import Step from "./components/steps/Step";
 
 function Todo() {
