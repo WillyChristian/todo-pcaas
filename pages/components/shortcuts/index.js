2b15e4d93b4f9e9ba7170643571539924cca8390
WillyChristian <crisaort@gmail.com>
2021-11-19 01:51:16 -0300

    change img tag form Next/image import

--- pages/components/shortcuts/index.js
+++ pages/components/shortcuts/index.js
@@ -5,12 +5,12 @@ const programs = require("./config.json");
 export default function Shortcut() {
   return (
     <div className="w-full h-full flex flex-col items-center">
-      <h2 class="my-2 text-xl font-medium text-gray-800 dark:text-white md:text-2xl">
+      <h2 className="my-2 text-xl font-medium text-gray-800 dark:text-white md:text-2xl">
         Atalhos de instalação
       </h2>
       <div className="flex flex-wrap justify-center items-center w-full m-1">
-        {programs["programs"].map((softwares) => {
-          return <Card software={softwares} />;
+        {programs["programs"].map((softwares, index) => {
+          return <Card key={index} software={softwares} />;
         })}
       </div>
     </div>
